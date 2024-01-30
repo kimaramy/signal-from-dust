db4de8e94053b300d5ce00b7c9d287d31e99dd4b
Haram Kim <fed.haram@gmail.com>
2024-01-30 14:58:54 +0900

    set openapi url to env variable

--- domains/realtime/services.ts
+++ domains/realtime/services.ts
@@ -1,25 +1,30 @@
 export async function fetchRealtimeDataset() {
-  const serviceName = 'ListAvgOfSeoulAirQualityService';
-  const response = await fetch(
-    `http://openapi.seoul.go.kr:8088/${process.env.NEXT_PUBLIC_SEOUL_OPENAPI_TOKEN}/json/${serviceName}/1/5/`,
-    {
+  const service = 'ListAvgOfSeoulAirQualityService';
+  const endpoint = `${process.env.NEXT_PUBLIC_SEOUL_OPENAPI_ORIGIN}/${process.env.NEXT_PUBLIC_SEOUL_OPENAPI_TOKEN}/json/${service}/1/5/`;
+  try {
+    const response = await fetch(endpoint, {
+      mode: 'no-cors',
       next: {
         revalidate: 3600,
         tags: [realtimeDatasetRevalidateTag],
       },
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch data'); // This will activate the closest `error.ts` Error Boundary
     }
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch data'); // This will activate the closest `error.ts` Error Boundary
+    // console.log(JSON.stringify(response.url, null, 2));
+    const body = await response.json();
+    const isSuccess = body[service]['RESULT']['CODE'] === 'INFO-000';
+    const dataset = body[service]['row'];
+    // console.log(`data: %s`, JSON.stringify(body, null, 2));
+    if (!isSuccess) {
+      throw new Error(JSON.stringify(body, null, 2)); // This will activate the closest `error.ts` Error Boundary
+    }
+    return dataset as RealtimeData[];
+  } catch (err) {
+    console.log(JSON.stringify(err, null, 2));
+    throw err;
   }
-  const body = await response.json();
-  const isSuccess = body[serviceName]['RESULT']['CODE'] === 'INFO-000';
-  const dataset = body[serviceName]['row'];
-  // console.log(`data: %s`, JSON.stringify(body, null, 2));
-  if (!isSuccess) {
-    throw new Error(JSON.stringify(body, null, 2)); // This will activate the closest `error.ts` Error Boundary
-  }
-  return dataset as RealtimeData[];
 }
 
 export const realtimeDatasetRevalidateTag = 'realtime';
--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_SUPABASE_URL: string;
     readonly NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
     readonly NEXT_PUBLIC_SEOUL_OPENAPI_TOKEN: string;
+    readonly NEXT_PUBLIC_SEOUL_OPENAPI_ORIGIN: string;
   }
 }
