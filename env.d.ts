09dfdb94868cfafa8bd3cd542955f094c7dc6630
Haram Kim <fed.haram@gmail.com>
2024-02-06 21:28:28 +0900

    server-only envs to .env.local & universal envs to .env

--- env.d.ts
+++ env.d.ts
@@ -3,7 +3,7 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_URL: string;
     readonly NEXT_PUBLIC_SUPABASE_URL: string;
     readonly NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    readonly NEXT_PUBLIC_SEOUL_OPENAPI_URL: string;
-    readonly NEXT_PUBLIC_SEOUL_OPENAPI_KEY: string;
+    readonly SEOUL_OPENAPI_URL: string;
+    readonly SEOUL_OPENAPI_KEY: string;
   }
 }
