5d926e73ef0e6252ac686c27d8db1e43f3131939
Haram Kim <fed.haram@gmail.com>
2024-02-08 17:43:13 +0900

    support query params filtering

--- components/dataset/components/RealtimeDataset.hooks.ts
+++ components/dataset/components/RealtimeDataset.hooks.ts
@@ -1,8 +1,16 @@
+import { DustUtils, LocationUtils } from '@/lib/model';
+import { useFilterQueryParamsEffect } from '@/lib/router';
 import { useDustKey } from '@/components/dust';
 import { useLocationKey } from '@/components/location';
 
 export function useRealtimeDatasetParams() {
   const locationKey = useLocationKey('query');
   const dustKey = useDustKey('query');
+
+  useFilterQueryParamsEffect({
+    method: 'pick',
+    names: [LocationUtils.schema.name, DustUtils.schema.name],
+  });
+
   return { locationKey, dustKey };
 }
