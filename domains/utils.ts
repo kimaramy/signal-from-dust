71d3331bf91ab9dd0acd7ef420c81abb756b0ab7
Haram Kim <fed.haram@gmail.com>
2024-02-10 20:09:42 +0900

    adjust abort timeout

--- domains/utils.ts
+++ domains/utils.ts
@@ -3,7 +3,7 @@ export type FetchOptions = { signal?: AbortSignal };
 export const defaultAbortController = new AbortController();
 
 const defualtFetchOptions = Object.freeze({
-  signal: AbortSignal.timeout(10000), // 10 seconds
+  signal: AbortSignal.timeout(30000), // max 30 seconds
 } satisfies FetchOptions);
 
 export const getFetchOptions = (options?: FetchOptions) => ({
